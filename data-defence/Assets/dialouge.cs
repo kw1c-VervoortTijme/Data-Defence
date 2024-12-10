c8d123810b22cf7fbdee1c1dbead489198403aee
kw1c-VervoortTijme <tijme.vervoort@edu-kw1c.nl>
2024-12-10 12:22:17 +0100

    main menu
     main menu added

--- data-defence/Assets/dialouge.cs
+++ data-defence/Assets/dialouge.cs
@@ -1,66 +1,87 @@
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI; // For handling UI components like buttons
 using System.Collections;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class DialogueManager : MonoBehaviour
 {
-    public TextMeshProUGUI textcomponent;
-    public string[] lines;
-    public float textspeed;
+    public GameObject dialoguePanel;         // The dialogue panel object
+    public TextMeshProUGUI textComponent;    // The TextMeshPro text component for dialogue
+    public Button startButton;              // The Start button from the main menu
+    public Button exitButton;               // The Exit button from the main menu
+    public string[] lines;                  // The array of dialogue lines
+    public float textSpeed;                 // Speed for the typing effect
 
     private int index;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        textcomponent.text = string.Empty;
-        StartDialougue();
+        // Initially hide the dialogue panel
+        dialoguePanel.SetActive(false);
+
+        // Add listeners to the buttons
+        startButton.onClick.AddListener(StartDialogue);
+        exitButton.onClick.AddListener(ExitGame);
     }
 
-    // Update is called once per frame
+    public void StartDialogue()
+    {
+        // Hide the Start and Exit buttons when dialogue begins
+        startButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+
+        // Show the dialogue panel and start typing the first line
+        dialoguePanel.SetActive(true);
+        textComponent.text = string.Empty;
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting game...");
+        Application.Quit(); // Quits the application
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // If the dialogue panel is active, allow skipping or advancing lines
+        if (dialoguePanel.activeSelf && Input.GetMouseButtonDown(0))
         {
-            if (textcomponent.text == lines[index])
+            if (textComponent.text == lines[index])
             {
-                Nextline();
+                NextLine();
             }
             else
             {
                 StopAllCoroutines();
-                textcomponent.text = lines[index];
+                textComponent.text = lines[index];
             }
         }
     }
 
-    void StartDialougue()
-    {
-        index = 0;
-        StartCoroutine(Typeline());
-    }
-    IEnumerator Typeline()
+    IEnumerator TypeLine()
     {
         foreach (char c in lines[index].ToCharArray())
         {
-            textcomponent.text += c;
-            yield return new WaitForSeconds(textspeed);
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
         }
-
     }
-    void Nextline()
+
+    void NextLine()
     {
         if (index < lines.Length - 1)
         {
             index++;
-            textcomponent.text = string.Empty;
-            StartCoroutine(Typeline());
-
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
         }
         else
         {
-            gameObject.SetActive(false);
+            // End of dialogue
+            dialoguePanel.SetActive(false);
+            Debug.Log("Dialogue finished. Game starts here...");
         }
     }
 }
